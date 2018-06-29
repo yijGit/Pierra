c3d10afa86d948af60a5000826e3b6ad7f2188d5
notemaster <notemasterisaac@gmail.com>
2018-06-29 09:26:22 -0400

    update

--- vm.py
+++ vm.py
@@ -5,7 +5,8 @@ This version should work on both Python 2 and 3.
 """
 
 from collections import deque
-
+import sys
+import os
 
 class Stack(deque):
     push = deque.append
@@ -162,13 +163,14 @@ class Machine:
         self.push(self.CX)
 
     def not0(self):
-        binary = hex(self.CX)[2:]
-        binary = binary.charAt(0) + (binary.charAt(1) & hex.f)  # 32 bit representation
+        binary = bin(self.CX)[2:]
+        binary = binary.charAt(0) + (binary.charAt(1) ^)  # 32 bit representation
         self.push(binary)
 
     def shl(self):
         binary = hex(self.CX)[2:]
         binary = binary << 1
+        self.CX = binary
         self.push(binary)
 
     def ifz(self):
@@ -184,13 +186,19 @@ class Machine:
             self.instruction_pointer += 1
 
     def jmp(self):  # read next four noop instructions: Assume Template
-        template = self.code[2 * self.instruction_pointer + 1: 2 * self.instruction_pointer + 10]  # Trying to find template
-        self.instruction_pointer = self.compl(template, 1)
+        template = self.code[2 * self.instruction_pointer + 2: 2 * self.instruction_pointer + 11]  # Trying to find template
+        if self.test(template):
+            self.instruction_pointer = self.compl(template, 1)
+        else:
+            self.instruction_pointer = self.AX
 
 
     def jmpb(self):  # Read Last four noop instructions: Assume Template
         template = self.code[2 * self.instruction_pointer + 1: 2 * self.instruction_pointer + 10]  # Trying to find template
-        self.instruction_pointer = self.compl(template, 0)
+        if self.test(template):
+            self.instruction_pointer = self.compl(template, 0)
+        else:
+            self.instruction_pointer = self.AX
 
 
     def compl(self, template, dir):
@@ -214,10 +222,16 @@ class Machine:
         return self.instruction_pointer
     def test(self, template):
         for i in range(len(template)):
-            if i is not 0 or 1:
-                return False
+            if i % 2 != 0:
+                if i is not 0 or 1:
+                    return False
+                else:
+                    return True
             else:
-                return True
+                if i is not 0:
+                    return False
+                else:
+                    return True
 
     def call(self):
         self.push(self.instruction_pointer + 1)
@@ -238,9 +252,19 @@ class Machine:
     def divide(self):
         pass
 
+    def main(self):
+        self.get_soup()
+        self.life()
+        self.write_soup()
+    def life(self):
+        pass
 
-def if_stmt(self):
-    false_clause = self.pop()
-    true_clause = self.pop()
-    test = self.pop()
-    self.push(true_clause if test else false_clause)
+    def time_slice(self, ci):
+        ce = None
+        i = ''
+        di, j, size_slice = 0
+        ce = cells + ci
+        for j in range(size_slice):
+            i = self.fetch(ce->c.ip)
+            di = decode(i)
+            execute
